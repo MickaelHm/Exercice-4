762d5da3b7276d0edb5425cd007b3b8e54d3c64e
MickaelHm <harmeau.mickael@gmail.com>
2018-12-17 14:48:52 +0100

    Exercice complet

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = "Monsieur";
+            string name = "Vincent";
+            int date = 1;
+            Console.WriteLine("Bonjour " + type +" " + name + " nous somme le " + date + " du mois, comment allez-vous ?");
+        }
+    }
+}
